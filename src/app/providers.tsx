97e9ff8b72b89be0aa4d4d562fc828f0f2975af9
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-11-11 15:44:17 +0100

    Add react query dev tools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,11 +4,13 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { PropsWithChildren } from 'react'
 import { Theme } from 'react-daisyui'
 import { ClientContextProvider } from '@/app/client-context'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const client = new QueryClient()
 
 export const Providers = ({ children }: PropsWithChildren) => (
   <QueryClientProvider client={client}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <ClientContextProvider>
       <Theme dataTheme="dark">{children}</Theme>
     </ClientContextProvider>
